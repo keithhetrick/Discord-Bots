645b1f0c845af97f8996522cd32c069cea4cee98
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-10-08 12:33:51 -0500

    cleverbot AI added for chatbox functionality

--- TCS-Bot/index.js
+++ TCS-Bot/index.js
@@ -1,3 +1,4 @@
+const cleverbot = require("cleverbot-free");
 const { Client, GatewayIntentBits } = require("discord.js");
 require("dotenv").config();
 
@@ -16,6 +17,24 @@ client.on("ready", () => {
 client.on("messageCreate", (message) => {
   if (message.content === "ping") {
     message.reply("pong");
+  }
+});
+
+// Cleverbot AI response & add text / res to Discord
+let conversation = [];
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return false;
+  if (message.mentions.has(client.user.id)) {
+    let text = message.content;
+    text = text.substring(text.indexOf(">") + 2, text.length);
+    console.log(text);
+
+    cleverbot(text, conversation).then((res) => {
+      conversation.push(text);
+      conversation.push(res);
+      message.channel.send(res);
+    });
   }
 });
 
